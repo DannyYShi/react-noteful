30e596d31049ea5dd5301ed1aa55f6a2f71c4ff4
Danny Shi <dannyshi.sr@gmail.com>
2020-09-29 11:58:46 -0400

    finished error boundaries and proptypes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import { FileContextProvider } from "./FileContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./Header";
+import FoldersSidebar from "./FoldersSidebar";
+import NoteSidebar from "./Note/NoteSidebar";
+import NotesList from "./NotesList";
+import NoteDetails from "./Note/NoteDetails";
+
+import AddFolder from "./Folder/AddFolder";
+import AddNote from "./Note/AddNote";
+
+import ErrorPage from "./ErrorPage";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header></Header>
+        <ErrorPage>
+          <FileContextProvider>
+            <div className="body">
+              <div className="sidebar">
+                <Route path="/" exact component={FoldersSidebar} />
+                <Route path="/add/folder" exact component={FoldersSidebar} />
+                <Route path="/add/note" exact component={FoldersSidebar} />
+                <Route
+                  path="/folder/:folderName"
+                  exact
+                  component={FoldersSidebar}
+                />
+                <Route
+                  path="/folder/:folderName/:noteName"
+                  render={(routeProps) => (
+                    <NoteSidebar routeProps={routeProps} />
+                  )}
+                />
+              </div>
+              <main>
+                <Route path="/" exact component={NotesList} />
+                <Route path="/add/folder" exact component={AddFolder} />
+                <Route path="/add/note" exact component={AddNote} />
+                <Route
+                  path="/folder/:folderName"
+                  exact
+                  render={(routeProps) => <NotesList routeProps={routeProps} />}
+                />
+                <Route
+                  path="/folder/:folderName/:noteName"
+                  render={(routeProps) => (
+                    <NoteDetails routeProps={routeProps} />
+                  )}
+                />
+              </main>
+            </div>
+          </FileContextProvider>
+        </ErrorPage>
+      </>
+    );
+  }
 }
 
 export default App;
